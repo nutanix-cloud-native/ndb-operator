48aa4646f200df4f4f150ea082924ff28c061083
ajithkvp <ajithkvp.2010@gmail.com>
2023-03-21 18:12:08 -0400

    bug fix: added NewDbServerTimeZone field to mongo provisioning payload

--- api/v1alpha1/ndb_api_helpers.go
+++ api/v1alpha1/ndb_api_helpers.go
@@ -30,131 +30,6 @@ import (
 
 // This function generates and returns a request for provisioning a database (and a dbserver vm) on NDB
 // The database provisioned has a NONE time machine SLA attached to it, and uses the default OOB profiles
-// func GenerateProvisioningRequest(ctx context.Context, ndbclient *ndbclient.NDBClient, dbSpec DatabaseSpec, reqData map[string]interface{}) (req *DatabaseProvisionRequest, err error) {
-// 	log := ctrllog.FromContext(ctx)
-// 	log.Info("Entered ndb_api_helpers.GenerateProvisioningRequest", "database name", dbSpec.Instance.DatabaseInstanceName, "database type", dbSpec.Instance.Type)
-
-// 	// Fetching the NONE TM SLA
-// 	sla, err := GetNoneTimeMachineSLA(ctx, ndbclient)
-// 	if err != nil {
-// 		log.Error(err, "Error occurred while getting NONE TM SLA", "database name", dbSpec.Instance.DatabaseInstanceName, "database type", dbSpec.Instance.Type)
-// 		return
-// 	}
-
-// 	// Fetch the OOB profiles for the database
-// 	profilesMap, err := GetOOBProfiles(ctx, ndbclient, dbSpec.Instance.Type)
-// 	if err != nil {
-// 		log.Error(err, "Error occurred while getting OOB profiles", "database name", dbSpec.Instance.DatabaseInstanceName, "database type", dbSpec.Instance.Type)
-// 		return
-// 	}
-
-// 	database_names := strings.Join(dbSpec.Instance.DatabaseNames, ",")
-
-// 	// Type assertion
-// 	dbPassword, ok := reqData[NDB_PARAM_PASSWORD].(string)
-// 	if !ok || dbPassword == "" {
-// 		err = errors.New("invalid database password")
-// 		var errStatement string
-// 		if !ok {
-// 			errStatement = "Type assertion failed for database password. Expected a string value"
-// 		} else {
-// 			errStatement = "Empty database password"
-// 		}
-// 		log.Error(err, errStatement)
-// 	}
-// 	// Type assertion
-// 	SSHPublicKey, ok := reqData[NDB_PARAM_SSH_PUBLIC_KEY].(string)
-// 	if !ok || SSHPublicKey == "" {
-// 		err = errors.New("invalid ssh public key")
-// 		var errStatement string
-// 		if !ok {
-// 			errStatement = "Type assertion failed for SSHPublicKey. Expected a string value"
-// 		} else {
-// 			errStatement = "Empty SSHPublicKey"
-// 		}
-// 		log.Error(err, errStatement)
-// 	}
-
-// 	// Creating a provisioning request based on the database type
-// 	req = &DatabaseProvisionRequest{
-// 		DatabaseType:             GetDatabaseEngineName(dbSpec.Instance.Type),
-// 		Name:                     dbSpec.Instance.DatabaseInstanceName,
-// 		DatabaseDescription:      "Database provisioned by ndb-operator: " + dbSpec.Instance.DatabaseInstanceName,
-// 		SoftwareProfileId:        profilesMap[PROFILE_TYPE_SOFTWARE].Id,
-// 		SoftwareProfileVersionId: profilesMap[PROFILE_TYPE_SOFTWARE].LatestVersionId,
-// 		ComputeProfileId:         profilesMap[PROFILE_TYPE_COMPUTE].Id,
-// 		NetworkProfileId:         profilesMap[PROFILE_TYPE_NETWORK].Id,
-// 		DbParameterProfileId:     profilesMap[PROFILE_TYPE_DATABASE_PARAMETER].Id,
-// 		NewDbServerTimeZone:      dbSpec.Instance.TimeZone,
-// 		CreateDbServer:           true,
-// 		NodeCount:                1,
-// 		NxClusterId:              dbSpec.NDB.ClusterId,
-// 		SSHPublicKey:             SSHPublicKey,
-// 		Clustered:                false,
-// 		AutoTuneStagingDrive:     true,
-// 		TimeMachineInfo: TimeMachineInfo{
-// 			Name:             dbSpec.Instance.DatabaseInstanceName + "_TM",
-// 			Description:      sla.Description,
-// 			SlaId:            sla.Id,
-// 			Schedule:         make(map[string]string),
-// 			Tags:             make([]string, 0),
-// 			AutoTuneLogDrive: true,
-// 		},
-// 		ActionArguments: []ActionArgument{
-// 			{
-// 				Name:  "proxy_read_port",
-// 				Value: "5001",
-// 			},
-// 			{
-// 				Name:  "listener_port",
-// 				Value: "5432",
-// 			},
-// 			{
-// 				Name:  "proxy_write_port",
-// 				Value: "5000",
-// 			},
-// 			{
-// 				Name:  "database_size",
-// 				Value: strconv.Itoa(dbSpec.Instance.Size),
-// 			},
-// 			{
-// 				Name:  "auto_tune_staging_drive",
-// 				Value: "true",
-// 			},
-// 			{
-// 				Name:  "enable_synchronous_mode",
-// 				Value: "false",
-// 			},
-// 			{
-// 				Name:  "backup_policy",
-// 				Value: "primary_only",
-// 			},
-// 			{
-// 				Name:  "dbserver_description",
-// 				Value: "dbserver for " + dbSpec.Instance.DatabaseInstanceName,
-// 			},
-// 			{
-// 				Name:  "database_names",
-// 				Value: database_names,
-// 			},
-// 			{
-// 				Name:  "db_password",
-// 				Value: dbPassword,
-// 			},
-// 		},
-// 		Nodes: []Node{
-// 			{
-// 				Properties: make([]string, 0),
-// 				VmName:     dbSpec.Instance.DatabaseInstanceName + "_VM",
-// 			},
-// 		},
-// 	}
-// 	log.Info("Returning from ndb_api_helpers.GenerateProvisioningRequest", "database name", dbSpec.Instance.DatabaseInstanceName, "database type", dbSpec.Instance.Type)
-// 	return
-// }
-
-// This function generates and returns a request for provisioning a database (and a dbserver vm) on NDB
-// The database provisioned has a NONE time machine SLA attached to it, and uses the default OOB profiles
 func GenerateProvisioningRequest(ctx context.Context, ndbclient *ndbclient.NDBClient, dbSpec DatabaseSpec, reqData map[string]interface{}) (req *DatabaseProvisionRequest, err error) {
 	log := ctrllog.FromContext(ctx)
 	log.Info("Entered ndb_api_helpers.GenerateProvisioningRequest", "database name", dbSpec.Instance.DatabaseInstanceName, "database type", dbSpec.Instance.Type)
@@ -210,6 +85,7 @@ func GenerateProvisioningRequest(ctx context.Context, ndbclient *ndbclient.NDBCl
 			ComputeProfileId:         profilesMap[PROFILE_TYPE_COMPUTE].Id,
 			NetworkProfileId:         profilesMap[PROFILE_TYPE_NETWORK].Id,
 			DbParameterProfileId:     profilesMap[PROFILE_TYPE_DATABASE_PARAMETER].Id,
+			NewDbServerTimeZone:      dbSpec.Instance.TimeZone,
 			CreateDbServer:           true,
 			NodeCount:                1,
 			NxClusterId:              dbSpec.NDB.ClusterId,
