44fb446a271737691a3a671b7d6db45b407e5bdd
Manav <manav.rajvanshi@nutanix.com>
2023-06-22 23:32:05 -0700

    Added, moved and refactored  unit tests

--- ndb_api/db_helpers_test.go
+++ ndb_api/db_helpers_test.go
@@ -12,9 +12,12 @@ import (
 
 // Test constants
 const (
-	TEST_PASSWORD = "testPassword"
-	TEST_SSHKEY   = "testSSHKey"
-	TEST_DB_NAMES = "testDB"
+	TEST_PASSWORD      = "testPassword"
+	TEST_SSHKEY        = "testSSHKey"
+	TEST_DB_NAMES      = "testDB"
+	TEST_TIMEZONE      = "test-timezone"
+	TEST_CLUSTER_ID    = "test-cluster-id"
+	TEST_INSTANCE_SIZE = 100
 )
 
 // Tests the validateReqData() function with different values of password and sshkey
@@ -445,6 +448,205 @@ func TestGenerateProvisioningRequest_WithoutValidTMDetails_ReturnsError(t *testi
 		mockDatabase.On("GetTMSchedule").Return(tc.tmSchedule, tc.tmScheduleErr)
 
 		_, err := GenerateProvisioningRequest(context.Background(), ndb_client, mockDatabase, reqData)
+
+		if err != tc.expectedError && err.Error() != tc.expectedError.Error() {
+			t.Fatalf("expected: %v, got: %v", tc.expectedError, err)
+		}
+	}
+}
+
+func TestGenerateProvisioningRequest_WithoutValidProfileResolvers_ReturnsError(t *testing.T) {
+	server := GetServerTestHelper(t)
+	defer server.Close()
+	ndb_client := ndb_client.NewNDBClient("username", "password", server.URL, "", true)
+
+	reqData := map[string]interface{}{
+		common.NDB_PARAM_PASSWORD:       TEST_PASSWORD,
+		common.NDB_PARAM_SSH_PUBLIC_KEY: TEST_SSHKEY,
+	}
+
+	getResolver := func(p ProfileResponse, e error) *MockProfileResolverInterface {
+		profileResolver := MockProfileResolverInterface{}
+		profileResolver.On("GetId").Return(p.Id)
+		profileResolver.On("GetName").Return(p.Name)
+		profileResolver.On("Resolve").Return(p, e)
+		return &profileResolver
+	}
+	softwareError := errors.New("test-error-software")
+	computeError := errors.New("test-error-compute")
+	networkError := errors.New("test-error-network")
+	dbParamError := errors.New("test-error-dbParam")
+	dbParamInstanceError := errors.New("test-error-dbParamInstance")
+
+	tests := []struct {
+		databaseType         string
+		softwareError        error
+		computeError         error
+		networkError         error
+		dbParamError         error
+		dbParamInstanceError error
+		expectedError        error
+	}{
+		{
+			softwareError: softwareError,
+			expectedError: softwareError,
+		},
+		{
+			computeError:  computeError,
+			expectedError: computeError,
+		},
+		{
+			networkError:  networkError,
+			expectedError: networkError,
+		},
+		{
+			dbParamError:  dbParamError,
+			expectedError: dbParamError,
+		},
+		{
+			databaseType:         common.DATABASE_TYPE_MSSQL,
+			dbParamInstanceError: dbParamInstanceError,
+			expectedError:        dbParamInstanceError,
+		},
+	}
+
+	for _, tc := range tests {
+
+		software := getResolver(ProfileResponse{}, tc.softwareError)
+		compute := getResolver(ProfileResponse{}, tc.computeError)
+		network := getResolver(ProfileResponse{}, tc.networkError)
+		dbParam := getResolver(ProfileResponse{}, tc.dbParamError)
+		dbParamInstance := getResolver(ProfileResponse{}, tc.dbParamInstanceError)
+
+		instanceType := tc.databaseType
+		if instanceType == common.DATABASE_TYPE_MSSQL {
+			software = &MockProfileResolverInterface{}
+			software.On("GetName").Return("test-mssql-software-profile-name")
+			software.On("GetId").Return("test-mssql-software-profile-id")
+			software.On("Resolve").Return(ProfileResponse{
+				Id:              "test-mssql-software-profile-id",
+				Name:            "test-mssql-software-profile-name",
+				Type:            common.PROFILE_TYPE_SOFTWARE,
+				EngineType:      common.DATABASE_ENGINE_TYPE_MSSQL,
+				LatestVersionId: "v-id-mssql",
+				Topology:        common.TOPOLOGY_SINGLE,
+				Status:          "READY",
+				SystemProfile:   false,
+			}, nil)
+		}
+
+		profileResolvers := ProfileResolvers{
+			common.PROFILE_TYPE_SOFTWARE:                    software,
+			common.PROFILE_TYPE_COMPUTE:                     compute,
+			common.PROFILE_TYPE_NETWORK:                     network,
+			common.PROFILE_TYPE_DATABASE_PARAMETER:          dbParam,
+			common.PROFILE_TYPE_DATABASE_PARAMETER_INSTANCE: dbParamInstance,
+		}
+
+		mockDatabase := MockDatabaseInterface{}
+		mockDatabase.On("GetDBInstanceName").Return("db_instance_name")
+		mockDatabase.On("GetDBInstanceType").Return(instanceType)
+		mockDatabase.On("GetTMDetails").Return("tm_name", "rm_description", "SLA 1")
+		mockDatabase.On("GetTMSchedule").Return(Schedule{}, nil)
+		mockDatabase.On("GetProfileResolvers").Return(profileResolvers)
+
+		_, err := GenerateProvisioningRequest(context.Background(), ndb_client, &mockDatabase, reqData)
+
+		if err != tc.expectedError && err.Error() != tc.expectedError.Error() {
+			t.Fatalf("expected: %v, got: %v", tc.expectedError, err)
+		}
+	}
+}
+
+func TestGenerateProvisioningRequest_AgainstDifferentReqData(t *testing.T) {
+	server := GetServerTestHelper(t)
+	defer server.Close()
+	ndb_client := ndb_client.NewNDBClient("username", "password", server.URL, "", true)
+
+	getResolver := func(p ProfileResponse, e error) *MockProfileResolverInterface {
+		profileResolver := MockProfileResolverInterface{}
+		profileResolver.On("GetId").Return(p.Id)
+		profileResolver.On("GetName").Return(p.Name)
+		profileResolver.On("Resolve").Return(p, e)
+		return &profileResolver
+	}
+
+	tests := []struct {
+		databaseType  string
+		reqData       map[string]interface{}
+		expectedError error
+	}{
+		{
+			databaseType: common.DATABASE_TYPE_POSTGRES,
+			reqData: map[string]interface{}{
+				common.NDB_PARAM_PASSWORD:       "",
+				common.NDB_PARAM_SSH_PUBLIC_KEY: TEST_SSHKEY,
+			},
+			expectedError: errors.New("invalid database password"),
+		},
+		{
+			databaseType: common.DATABASE_TYPE_MYSQL,
+			reqData: map[string]interface{}{
+				common.NDB_PARAM_PASSWORD:       TEST_PASSWORD,
+				common.NDB_PARAM_SSH_PUBLIC_KEY: "",
+			},
+			expectedError: errors.New("invalid ssh public key"),
+		},
+		{
+			databaseType: common.DATABASE_TYPE_MSSQL,
+			reqData: map[string]interface{}{
+				common.NDB_PARAM_PASSWORD:       TEST_PASSWORD,
+				common.NDB_PARAM_SSH_PUBLIC_KEY: TEST_SSHKEY,
+			},
+			expectedError: nil,
+		},
+	}
+
+	for _, tc := range tests {
+
+		software := getResolver(ProfileResponse{}, nil)
+		compute := getResolver(ProfileResponse{}, nil)
+		network := getResolver(ProfileResponse{}, nil)
+		dbParam := getResolver(ProfileResponse{}, nil)
+		dbParamInstance := getResolver(ProfileResponse{}, nil)
+
+		instanceType := tc.databaseType
+		if instanceType == common.DATABASE_TYPE_MSSQL {
+			software = &MockProfileResolverInterface{}
+			software.On("GetName").Return("test-mssql-software-profile-name")
+			software.On("GetId").Return("test-mssql-software-profile-id")
+			software.On("Resolve").Return(ProfileResponse{
+				Id:              "test-mssql-software-profile-id",
+				Name:            "test-mssql-software-profile-name",
+				Type:            common.PROFILE_TYPE_SOFTWARE,
+				EngineType:      common.DATABASE_ENGINE_TYPE_MSSQL,
+				LatestVersionId: "v-id-mssql",
+				Topology:        common.TOPOLOGY_SINGLE,
+				Status:          "READY",
+				SystemProfile:   false,
+			}, nil)
+		}
+
+		profileResolvers := ProfileResolvers{
+			common.PROFILE_TYPE_SOFTWARE:                    software,
+			common.PROFILE_TYPE_COMPUTE:                     compute,
+			common.PROFILE_TYPE_NETWORK:                     network,
+			common.PROFILE_TYPE_DATABASE_PARAMETER:          dbParam,
+			common.PROFILE_TYPE_DATABASE_PARAMETER_INSTANCE: dbParamInstance,
+		}
+
+		mockDatabase := MockDatabaseInterface{}
+		mockDatabase.On("GetDBInstanceName").Return("db_instance_name")
+		mockDatabase.On("GetDBInstanceType").Return(instanceType)
+		mockDatabase.On("GetTMDetails").Return("tm_name", "rm_description", "SLA 1")
+		mockDatabase.On("GetTMSchedule").Return(Schedule{}, nil)
+		mockDatabase.On("GetProfileResolvers").Return(profileResolvers)
+		mockDatabase.On("GetDBInstanceTimeZone").Return(TEST_TIMEZONE)
+		mockDatabase.On("GetNDBClusterId").Return(TEST_CLUSTER_ID)
+		mockDatabase.On("GetDBInstanceSize").Return(TEST_INSTANCE_SIZE)
+		mockDatabase.On("GetDBInstanceDatabaseNames").Return(TEST_DB_NAMES)
+
+		_, err := GenerateProvisioningRequest(context.Background(), ndb_client, &mockDatabase, tc.reqData)
 
 		if err != tc.expectedError && err.Error() != tc.expectedError.Error() {
 			t.Fatalf("expected: %v, got: %v", tc.expectedError, err)
--- ndb_api/interface_mock_test.go
+++ ndb_api/interface_mock_test.go
@@ -1,6 +1,8 @@
 package ndb_api
 
 import (
+	"context"
+
 	"github.com/stretchr/testify/mock"
 )
 
@@ -14,56 +16,74 @@ type MockProfileResolverInterface struct {
 	mock.Mock
 }
 
-// GetDBInstanceName is a mock implementation of the GetDBInstanceName method
+// GetDBInstanceName is a mock implementation of the GetDBInstanceName method in the Database interface
 func (m *MockDatabaseInterface) GetDBInstanceName() string {
 	args := m.Called()
 	return args.String(0)
 }
 
-// GetDBInstanceType is a mock implementation of the GetDBInstanceType method
+// GetDBInstanceType is a mock implementation of the GetDBInstanceType method in the Database interface
 func (m *MockDatabaseInterface) GetDBInstanceType() string {
 	args := m.Called()
 	return args.String(0)
 }
 
-// GetDBInstanceDatabaseNames is a mock implementation of the GetDBInstanceDatabaseNames method
+// GetDBInstanceDatabaseNames is a mock implementation of the GetDBInstanceDatabaseNames method in the Database interface
 func (m *MockDatabaseInterface) GetDBInstanceDatabaseNames() string {
 	args := m.Called()
 	return args.String(0)
 }
 
-// GetDBInstanceTimeZone is a mock implementation of the GetDBInstanceTimeZone method
+// GetDBInstanceTimeZone is a mock implementation of the GetDBInstanceTimeZone method in the Database interface
 func (m *MockDatabaseInterface) GetDBInstanceTimeZone() string {
 	args := m.Called()
 	return args.String(0)
 }
 
-// GetDBInstanceSize is a mock implementation of the GetDBInstanceSize method
+// GetDBInstanceSize is a mock implementation of the GetDBInstanceSize method in the Database interface
 func (m *MockDatabaseInterface) GetDBInstanceSize() int {
 	args := m.Called()
 	return args.Int(0)
 }
 
-// GetNDBClusterId is a mock implementation of the GetNDBClusterId method
+// GetNDBClusterId is a mock implementation of the GetNDBClusterId method in the Database interface
 func (m *MockDatabaseInterface) GetNDBClusterId() string {
 	args := m.Called()
 	return args.String(0)
 }
 
-// GetProfileResolvers is a mock implementation of the GetProfileResolvers method
+// GetProfileResolvers is a mock implementation of the GetProfileResolvers method in the Database interface
 func (m *MockDatabaseInterface) GetProfileResolvers() ProfileResolvers {
 	args := m.Called()
 	return args.Get(0).(ProfileResolvers)
 }
 
-// GetTMDetails is a mock implementation of the GetTMDetails method
+// GetTMDetails is a mock implementation of the GetTMDetails method in the Database interface
 func (m *MockDatabaseInterface) GetTMDetails() (string, string, string) {
 	args := m.Called()
 	return args.String(0), args.String(1), args.String(2)
 }
 
-// GetTMDetails is a mock implementation of the GetTMDetails method
+// GetTMSchedule is a mock implementation of the GetTMSchedule method in the Database interface
 func (m *MockDatabaseInterface) GetTMSchedule() (Schedule, error) {
 	args := m.Called()
-	return args.Get(0).(Schedule), args.Get(1).(error)
+	return args.Get(0).(Schedule), args.Error(1)
+}
+
+// GetName is a mock implementation of the GetName method defined in the ProfileResolver interface
+func (m *MockProfileResolverInterface) GetName() string {
+	args := m.Called()
+	return args.String(0)
+}
+
+// GetId is a mock implementation of the GetId method defined in the ProfileResolver interface
+func (m *MockProfileResolverInterface) GetId() string {
+	args := m.Called()
+	return args.String(0)
+}
+
+// Resolve is a mock implementation of the Resolve method defined in the ProfileResolver interface
+func (m *MockProfileResolverInterface) Resolve(ctx context.Context, allProfiles []ProfileResponse, filter func(p ProfileResponse) bool) (ProfileResponse, error) {
+	args := m.Called()
+	return args.Get(0).(ProfileResponse), args.Error(1)
 }
