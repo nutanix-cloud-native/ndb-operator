25e77859fbcdc65408b1504b2f6e74f29c4ea981
Mazin S <mazinshaaeldin@gmail.com>
2023-07-26 08:58:46 -0700

    Changed comments

--- automation/SetupInfo.go
+++ automation/SetupInfo.go
@@ -18,12 +18,12 @@ type SetupInfo struct {
 func (i *SetupInfo) getDbSecret() (*corev1.Secret, error) {
 	dbSecretbytes, err := readYAMLFile(i.dbSecretPath)
 	if err != nil {
-		log.Printf("errorr occurred while reading bytes from %s", i.dbSecretPath)
+		log.Printf("Error occurred while reading bytes from %s", i.dbSecretPath)
 		return nil, err
 	}
 	dbSecret, err := ConvertToSecret(dbSecretbytes)
 	if err != nil {
-		log.Printf("errorr occurred while converting bytes to secret")
+		log.Printf("Error occurred while converting bytes to secret")
 		return nil, err
 	}
 	return dbSecret, nil
@@ -32,12 +32,12 @@ func (i *SetupInfo) getDbSecret() (*corev1.Secret, error) {
 func (i *SetupInfo) getNdbSecret() (*corev1.Secret, error) {
 	ndbSecretbytes, err := readYAMLFile(i.ndbSecretPath)
 	if err != nil {
-		log.Printf("errorr occurred while reading bytes from %s", i.ndbSecretPath)
+		log.Printf("Error occurred while reading bytes from %s", i.ndbSecretPath)
 		return nil, err
 	}
 	ndbSecret, err := ConvertToSecret(ndbSecretbytes)
 	if err != nil {
-		log.Printf("errorr occurred while converting bytes to NdbSecret")
+		log.Printf("Error occurred while converting bytes to NdbSecret")
 		return nil, err
 	}
 	return ndbSecret, nil
@@ -46,12 +46,12 @@ func (i *SetupInfo) getNdbSecret() (*corev1.Secret, error) {
 func (i *SetupInfo) getDatabase() (*ndbv1alpha1.Database, error) {
 	databaseBytes, err := readYAMLFile(i.dbPath)
 	if err != nil {
-		log.Printf("errorr occurred while reading bytes from %s", i.dbPath)
+		log.Printf("Error occurred while reading bytes from %s", i.dbPath)
 		return nil, err
 	}
 	database, err := ConvertToDatabase(databaseBytes)
 	if err != nil {
-		log.Printf("errorr occurred while converting bytes to Database")
+		log.Printf("Error occurred while converting bytes to Database")
 		return nil, err
 	}
 	return database, nil
@@ -60,12 +60,12 @@ func (i *SetupInfo) getDatabase() (*ndbv1alpha1.Database, error) {
 func (i *SetupInfo) getAppPod() (*corev1.Pod, error) {
 	appPodBytes, err := readYAMLFile(i.appPodPath)
 	if err != nil {
-		log.Printf("errorr occurred while reading bytes from %s", i.appPodPath)
+		log.Printf("Error occurred while reading bytes from %s", i.appPodPath)
 		return nil, err
 	}
 	appPod, err := ConvertToPod(appPodBytes)
 	if err != nil {
-		log.Printf("errorr occurred while converting bytes to AppPod")
+		log.Printf("Error occurred while converting bytes to AppPod")
 		return nil, err
 	}
 	return appPod, nil
@@ -74,12 +74,12 @@ func (i *SetupInfo) getAppPod() (*corev1.Pod, error) {
 func (i *SetupInfo) getAppService() (*corev1.Service, error) {
 	appSvcBytes, err := readYAMLFile(i.appSvcPath)
 	if err != nil {
-		log.Printf("errorr occurred while reading bytes from %s", i.appSvcPath)
+		log.Printf("Error occurred while reading bytes from %s", i.appSvcPath)
 		return nil, err
 	}
 	appSvc, err := ConvertToService(appSvcBytes)
 	if err != nil {
-		log.Printf("errorr occurred while converting bytes to AppService")
+		log.Printf("Error occurred while converting bytes to AppService")
 		return nil, err
 	}
 	return appSvc, nil
