ae09d20078ebbd5618a6d0b5eba75643f30c70ba
Mazin S <mazinshaaeldin@gmail.com>
2023-08-10 16:39:32 -0700

    changed file path

--- automation/constants.go
+++ automation/constants.go
@@ -1,9 +1,9 @@
 package automation
 
 const (
-	DATABASE_PATH   = "./database.yaml"
-	DB_SECRET_PATH  = "./db-secret.yaml"
-	NDB_SECRET_PATH = "./ndb-secret.yaml"
-	APP_POD_PATH    = "./pod.yaml"
-	APP_SVC_PATH    = "./service.yaml"
+	DATABASE_PATH   = "./config/database.yaml"
+	DB_SECRET_PATH  = "./config/db-secret.yaml"
+	NDB_SECRET_PATH = "./config/ndb-secret.yaml"
+	APP_POD_PATH    = "./config/pod.yaml"
+	APP_SVC_PATH    = "./config/service.yaml"
 )
