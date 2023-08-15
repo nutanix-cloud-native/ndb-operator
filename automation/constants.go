dccbbb6fc68b92df3432efb7ff38aae42a9d0cf1
Mazin S <mazinshaaeldin@gmail.com>
2023-08-15 14:15:41 -0700

    Automated test framework with postgres single instance e2e (#137)
    * Added basic automation
    
    * Updated teardown
    
    * Addressed review comments
    
    * Added minor comment to DatabaseInterface
    
    * Changed databaseInstanceName
    
    * added comments, and error check
    
    * Changed comments
    
    * Changed function names
    
    * Deleted some error handelining not needed
    
    * Setup Path
    
    * changed to setuppath
    
    * added setupconfig structure
    
    * Added setupConfig case to ConverBytesToType, ConvertBytesToSetUpConfig, and createSetupConfig
    
    * Removed simple comments
    
    * added sandbox for putting secrets
    
    * added comment to test suite
    
    * As suggested by Yash, method should not be exported
    
    * save changes
    
    * removed a log i was using to help me
    
    * Not needed methods I created
    
    * Helpers changed
    
    * Renamed SetupPaths
    
    * Added some logs and deleted some logs
    
    * Added generic function
    
    * Add public key
    
    * Changed makefile to test pg_si
    
    * Changed method names to capital
    
    * Restructured tests
    
    * Changed to test mssql_si
    
    * Added logs
    
    * rough copy of tests
    
    * changed yaml
    
    * mssql e2e support
    
    * constant files added
    
    * Hiding mssql 2e2 test
    
    * Removed mssql test
    
    * e2e postgres passes
    
    * Added profiles
    
    * delted line in git ignore
    
    * Error check added
    
    * Made make-test run whole automation file instead of indivdual test
    
    * Removed ssh
    
    * CreateTypeFromPath instead of CreateGeneric
    
    * Removed some logs
    
    * removed because not using right now, will include in next PR
    
    * changed make file test
    
    * Renamed from generic to theType
    
    * Added comments
    
    * Deleted a comment and added one
    
    * changed comment
    
    * Moved yaml files to pg_si test
    
    * Change yaml property names
    
    * Refactored comments and code structure
    
    * Added comments
    
    * Added more comments
    
    * Better commenting and spacing
    
    * .gitignore changed
    
    * Refactored config file names to make them consistent, added a const folder, and added a test folder
    
    * Changed makefile and .ignote to accoutn for extra tests folder
    
    * changed file path
    
    * Edited CreateTypeFromPath to throw and error if theType is not a pointer using reflect package
    
    * Elimnated unecessary line
    
    * Deleted readYamlFile function and using new os package because old method is depreciated
    
    * Addded 'Provisioning' in front of TestSetup and TestTeardown functions
    
    ---------
    
    Co-authored-by: Manav <manav.rajvanshi@nutanix.com>

---
+++ automation/constants.go
@@ -0,0 +1,9 @@
+package automation
+
+const (
+	DATABASE_PATH   = "./config/database.yaml"
+	DB_SECRET_PATH  = "./config/db-secret.yaml"
+	NDB_SECRET_PATH = "./config/ndb-secret.yaml"
+	APP_POD_PATH    = "./config/pod.yaml"
+	APP_SVC_PATH    = "./config/service.yaml"
+)
