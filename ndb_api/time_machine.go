d780af8a57d05a254f3ccbddec8b80a2691ab7dc
Mazin S <mazinshaaeldin@gmail.com>
2023-11-02 10:51:14 -0700

    Added take TM snapshot API

--- ndb_api/time_machine.go
+++ ndb_api/time_machine.go
@@ -68,3 +68,62 @@ func ProvisionClone(ctx context.Context, ndbClient *ndb_client.NDBClient, req *D
 	log.Info("Returning from ndb_api.ProvisionClone")
 	return
 }
+
+// Takes a snapshot for a time machine
+// Returns the task info summary response for the operation TODO
+func TakeSnapshotForTM(ctx context.Context, ndbClient *ndb_client.NDBClient, tmName string) (task TaskInfoSummaryResponse, err error) {
+	log := ctrllog.FromContext(ctx)
+	log.Info("Entered ndb_api.TakeSnapshotForTM")
+	if ndbClient == nil {
+		err = errors.New("nil reference")
+		log.Error(err, "Received nil ndbClient reference")
+		return
+	}
+	// Checking if id is empty, this is necessary to create a snapshot for the timemachine (/tms/{timemachine_id}/snapshots)
+	if tmName == "" {
+		err = fmt.Errorf("snapshot id is empty")
+		log.Error(err, "no snapshot id provided")
+		return
+	}
+	takeSnapshotPath := fmt.Sprintf("tms/%s/snapshots", tmName)
+	requestBody := &SnapshotRequest{
+		Name: "snapshot2",
+		SnapshotLcmConfig: SnapshotLcmConfig{
+			SnapshotLCMConfigDetailed: SnapshotLcmConfigDetailed{
+				ExpiryDetails: ExpiryDetails{
+					ExpiryDateTimezone: "Asia/Calcutta",
+					ExpireInDays:       "10",
+				},
+			},
+		},
+	}
+
+	res, err := ndbClient.Post(takeSnapshotPath, requestBody)
+	if err != nil || res == nil || res.StatusCode != http.StatusOK {
+		if err == nil {
+			if res != nil {
+				err = fmt.Errorf("POST %s responded with %d", takeSnapshotPath, res.StatusCode)
+			} else {
+				err = fmt.Errorf("POST %s responded with nil response", takeSnapshotPath)
+			}
+		}
+
+		log.Error(err, "Error occurred taking TM snapshot")
+		return
+	}
+
+	log.Info(fmt.Sprintf("POST %s", takeSnapshotPath), "HTTP status code", res.StatusCode)
+	body, err := io.ReadAll(res.Body)
+	defer res.Body.Close()
+	if err != nil {
+		log.Error(err, "Error occurred reading response.Body in TakeSnapshotForTM")
+		return
+	}
+	err = json.Unmarshal(body, &task)
+	if err != nil {
+		log.Error(err, "Error occurred trying to unmarshal.")
+		return
+	}
+	log.Info("Returning from ndb_api.TakeSnapshotForTM")
+	return
+}
