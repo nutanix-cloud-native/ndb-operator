9960d36141e6b3545c4952c81a7b151e6b41245e
rdhamal <rdhamal@ncsu.edu>
2023-11-28 22:40:01 -0500

    Implemented controller skeleton

--- controllers/snapshot_controller.go
+++ controllers/snapshot_controller.go
@@ -19,18 +19,25 @@ package controllers
 import (
 	"context"
 
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/tools/record"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/log"
+	ctrllog "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
 
 	ndbv1alpha1 "github.com/nutanix-cloud-native/ndb-operator/api/v1alpha1"
+	"github.com/nutanix-cloud-native/ndb-operator/common/util"
+	"github.com/nutanix-cloud-native/ndb-operator/ndb_client"
 )
 
 // SnapshotReconciler reconciles a Snapshot object
 type SnapshotReconciler struct {
 	client.Client
-	Scheme *runtime.Scheme
+	Scheme   *runtime.Scheme
+	recorder record.EventRecorder
 }
 
 //+kubebuilder:rbac:groups=ndb.nutanix.com,resources=snapshots,verbs=get;list;watch;create;update;patch;delete
@@ -47,16 +54,62 @@ type SnapshotReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.14.1/pkg/reconcile
 func (r *SnapshotReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	log := ctrllog.FromContext(ctx)
+	log.Info("<==============================Reconcile Started=============================>")
+	// Fetch the snapshot from the namespace
+	snapshot := &ndbv1alpha1.Snapshot{}
+	err := r.Get(ctx, req.NamespacedName, snapshot)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			// Request object not found, could have been deleted after reconcile request.
+			// Return and don't requeue
+			log.Info("Snapshot not found. Ignoring since object must be deleted")
+			return doNotRequeue()
+		}
+		// Error reading the object - requeue the request.
+		log.Error(err, "Failed to get Snapshot")
+		return requeueOnErr(err)
+	}
 
-	// TODO(user): your logic here
+	log.Info("Snapshot Status: " + util.ToString(snapshot.Status))
 
-	return ctrl.Result{}, nil
+	// Fetch the NDBServer resource from the namespace
+	ndbServer := &ndbv1alpha1.NDBServer{}
+	err = r.Get(ctx, req.NamespacedName, ndbServer)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			// Request object not found, could have been deleted after reconcile request.
+			// Return and don't requeue
+			log.Info("NDBServer resource not found. Ignoring since object must be deleted")
+			return doNotRequeue()
+		}
+		// Error reading the object - requeue the request.
+		log.Error(err, "Failed to get NDB")
+		return requeueOnErr(err)
+	}
+
+	NDBInfo := ndbServer.Spec
+	username, password, caCert, err := getNDBCredentialsFromSecret(ctx, r.Client, NDBInfo.CredentialSecret, req.Namespace)
+	if err != nil {
+		r.recorder.Eventf(snapshot, "Warning", EVENT_INVALID_CREDENTIALS, "Error: %s", err.Error())
+		return requeueOnErr(err)
+	}
+	if caCert == "" {
+		log.Info("Ca-cert not found, falling back to host's HTTPs certs.")
+	}
+	ndbClient := ndb_client.NewNDBClient(username, password, NDBInfo.Server, caCert, NDBInfo.SkipCertificateVerification)
+
+	return r.handleSync(ctx, snapshot, ndbClient, req, ndbServer)
 }
 
 // SetupWithManager sets up the controller with the Manager.
 func (r *SnapshotReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	//Create a new EventRecorder with the provided name
+	r.recorder = mgr.GetEventRecorderFor("snapshot-controller")
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&ndbv1alpha1.Snapshot{}).
+		WithEventFilter(predicate.GenerationChangedPredicate{}).
+		Owns(&corev1.Service{}).
+		Owns(&corev1.Endpoints{}).
 		Complete(r)
 }
