95857e4a1a812af1618803f0fc25b4cc62533c25
bobdoe945 <patel.kandarp923@gmail.com>
2024-04-21 21:42:34 -0400

    Node change

--- api/v1alpha1/webhook_suite_test.go
+++ api/v1alpha1/webhook_suite_test.go
@@ -616,7 +616,7 @@ var _ = Describe("Webhook Tests", func() {
 				secondaryProp := createDefaultNodeProperties("database", "secondary")
 				proxyProp := createDefaultNodeProperties("haproxy", "secondary")
 				clone.Spec.Clone.IsHighAvailability = true
-				clone.Spec.Clone.Nodes = []Node{
+				clone.Spec.Clone.Nodes = []&Node{
 					Node {
 						VmName: "VM1",
 						Properties: *primaryProp,
