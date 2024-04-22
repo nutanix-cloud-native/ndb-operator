6b108e6203564e6a7a08cea5fe47ba8adf3b86ec
Justin Orringer <justinorringer@gmail.com>
2024-04-21 21:29:11 -0400

    Type errors

--- api/v1alpha1/node_helpers.go
+++ api/v1alpha1/node_helpers.go
@@ -6,45 +6,42 @@ package v1alpha1
 
 import (
 	"fmt"
-	"reflect"
-	"strings"
 )
 
 var (
-	typeOptions = map[string]bool{"database": true, "haproxy": true}
-	roleOptions = map[string]bool{"primary": true, "secondary": true}
+	typeOptions     = map[string]bool{"database": true, "haproxy": true}
+	roleOptions     = map[string]bool{"primary": true, "secondary": true}
 	failoverOptions = map[string]bool{"Automatic": true, "Manual": true}
 )
 
-func ValidateNodes(nodes []Node, isHighAvailability bool) error {
-	if !isHighAvailability || len(nodes) == 0 {
+func ValidateNodes(nodes []*Node, isHighAvailability bool) error {
+	if !isHighAvailability || len(nodes) < 5 {
 		return nil // no nodes is valid?
 	}
-	
+
 	vmNames := make(map[string]bool) // for validating that vmnames are unique
 	for _, node := range nodes {
-		for _, np := range node.Properties {
-			if err := ValidateNodeProperties(np); err != nil {
-				return err
-			}
+		if err := ValidateNodeProperties(node.Properties); err != nil {
+			return err
 		}
 
-		if _, ok := vmNames[node.VMName]; ok {
-			return fmt.Errorf("vmName %s is already specified", np.VMName)
+		if _, ok := vmNames[node.VmName]; ok {
+			return fmt.Errorf("vmName %s is already specified", node.VmName)
 		}
-		vmNames[node.VMName] = true
+		vmNames[node.VmName] = true
 	}
 
 	return nil
 }
 
-func ValidateNodeProperties(np v1alpha1.NodeProperties) error {
+func ValidateNodeProperties(np NodeProperties) error {
 	if !typeOptions[np.NodeType] {
 		return fmt.Errorf("invalid NodeType in Node Properties: %s", np.NodeType)
 	}
 
 	if !roleOptions[np.Role] {
 		return fmt.Errorf("invalid Role in Node Properties: %s", np.Role)
+	}
 
 	if !failoverOptions[np.FailoverMode] {
 		return fmt.Errorf("invalid FailoverMode in Node Properties: %s", np.FailoverMode)
